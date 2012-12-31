ef416227622a8f4f99a1e9a313711d398ff9422d
Geoffrey Slinker <gslinker@ancestry.com>
2012-12-31 15:28:56 -0700

    Console app

---
+++ Source/Ancestry.QueryProcessor.ConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ancestry.QueryProcessor.ConsoleApp
+{
+    class Program
+    {
+        internal enum Command
+        {
+            None = 0,
+            Execute,
+            Stop
+        }
+
+        static void Main( string[] args )
+        {
+            Console.WriteLine( "Control Enter to Excecute or F5" );
+            Console.WriteLine( "Control C to Stop or Control F5" );
+
+            StringBuilder sb = new StringBuilder( );
+            bool done = false;
+
+            Command command = Command.None;
+
+            while( !done )
+            {
+                command = Command.None;
+
+                ConsoleKeyInfo info = Console.ReadKey( false );
+
+                switch(info.Key)
+                {
+                    case ConsoleKey.F5:
+                        break;
+
+                    case ConsoleKey.Enter:
+                        break;
+                }
+            }
+        }
+    }
+}
